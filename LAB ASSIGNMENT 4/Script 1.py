7017ee4444c986bfefb4b5469724f3cd9a6f052c
alicia87600 <aliciatoppo75@gmail.com>
2025-11-24 20:42:20 +0530

    Delete LAB ASSIGNMENT 4 directory
    delete

--- LAB ASSIGNMENT 4/Script 1.py
+++
@@ -1,8 +0,0 @@
-print("Script 1: Starting...")
-print("Script 1: Doing some work...")
-
-# Example action
-with open("output1.txt", "w") as f:
-    f.write("This is output from script 1.\n")
-
-print("Script 1: Finished!")
